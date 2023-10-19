480df57ff2bd171ab60ad16f22f8eb2beb20abc7
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-10-19 12:52:39 +0200

    feat: add selecter class to control the visualization

---
+++ js/SelectControl.js
@@ -0,0 +1,34 @@
+export class SelectControl {
+  constructor(dataArr) {
+    this.element = document.createElement('select');
+    this.optionsArr = [];
+    dataArr.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item;
+      option.innerText = item;
+      this.optionsArr.push(option);
+    })
+    return this;
+  }
+
+  render(place, formatFunc) {
+    document.querySelector(place).append(this.element);
+    this.optionsArr.forEach(item => {
+      item.innerText = formatFunc(item.innerText);
+      this.element.append(item);
+    })
+    this.listen();
+    return this;
+  }
+
+  listen() {
+    this.element.addEventListener('change', () => {
+      this.selectedValue = this.element.options[this.element.selectedIndex].value;
+    })
+  }
+
+  get value() {
+    return this.selectedValue;
+  }
+
+}
--- visualization.js
+++ visualization.js
@@ -1,6 +1,7 @@
 'use strict';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
+import {SelectControl} from "./js/SelectControl.js";
 import {Axis} from "./js/Axis.js";
 import {Range} from "./js/Range.js";
 import {Scale} from "./js/Scale.js";
@@ -8,6 +9,7 @@ import {Svg} from "./js/Svg.js";
 import {SvgGroup} from "./js/SvgGroup.js";
 import {Text} from "./js/Text.js";
 import {size} from "./js/size.js";
+const controls = document.querySelector('#controls');
 const dataPath = 'data/coins.json';
 const svg = new Svg(size.viz.width, size.viz.height).render('#container');
 const viz = new SvgGroup('viz').render(svg, size.margin.left, size.margin.top);
@@ -25,9 +27,14 @@ d3.json(dataPath).then(dataset => {
   new Axis(dateScale).render(svg, size.margin.left, (size.margin.top + size.height), 'bottom', 4);
   new Axis(priceScale).render(svg, size.margin.left,  size.margin.top, 'left', 5);
 
-  const bitcoin = data['bitcoin'];
-
-  renderViz(bitcoin, dateScale, priceScale);
+  const coinSelect = new SelectControl(Object.keys(data)).render('#controls', formatSelectOption);
+//-------
+  const but = document.querySelector('#container');
+  but.addEventListener('click', () => {
+    console.log(coinSelect.value);
+  })
+//-------
+  renderViz(data['bitcoin'], dateScale, priceScale);
 
 }).catch(error => {
   console.log(error);
@@ -57,4 +64,8 @@ function renderViz(data, x, y) {
   viz.append('path')
     .classed('line', true)
     .attr('d', line(data));
+}
+
+function formatSelectOption(optText) {
+  return optText.slice(0,1).toUpperCase() + optText.slice(1);
 }
